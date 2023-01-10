8a08682522d15a14df26977e14631f2559e52026
Danilo <danilodantasofc@gmail.com>
2023-01-10 10:03:03 -0300

    subindo projeto thymeleaf

---
+++ layout/src/test/java/br/com/danilodantas/LayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilodantas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
